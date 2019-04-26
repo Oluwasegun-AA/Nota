6230f10ff8d8c2ea4e2f05a9061439eb5278354c
shegsteham <oluwasegunadepoju@gmail.com>
2019-04-27 00:00:23 +0100

    remove unnecessary comments

--- app/src/main/java/com/example/oluwasegunadepoju/nota/NoteInfo.java
+++ app/src/main/java/com/example/oluwasegunadepoju/nota/NoteInfo.java
@@ -44,7 +44,7 @@ public final class NoteInfo {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o)
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
